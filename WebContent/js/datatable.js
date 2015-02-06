1b3d4771595d712a5fd1912a4a07e9126e5ea871
Steven Wu <steven.j.wu@gmail.com>
2015-02-06 19:28:19 +1030

    Separated javascript from html page.

---
+++ WebContent/js/datatable.js
@@ -0,0 +1,40 @@
+
+/*
+ *	The javascript serves for datatable html page. 
+ */
+
+// Bootgrid widget
+$("#grid").bootgrid({
+	selection : true,
+	multiSelect : true,
+	formatters: {
+		"link": function(column, row) {
+			return "<a href=\"www.google.com.au\">Google" + "</a>";
+		}
+	}
+}).on("selected.rs.jquery.bootgrid", function(e, rows) {
+	var rowIds = [];
+	for (var i = 0; i < rows.length; i++) {
+		rowIds.push(rows[i].id);
+	}
+	// Pass selected rowIds from bootgrid widget to another javascript
+	// function
+	print(rowIds);
+});
+
+function print(rowIds) {
+	console.log("Printing selected row ID(s): " + rowIds.join(","));
+}
+
+$(function() {
+	$("#drop-down").change(function() {
+		var selected = $("#drop-down").val();
+		console.log("selected item in drop-down: " + selected);
+	});
+});
+
+function getCellValue(column, row) {
+	var columnIndex = $("#" + column).index();
+	var row = $("#" + row);
+	return row.find("td").eq(columnIndex);
+}
