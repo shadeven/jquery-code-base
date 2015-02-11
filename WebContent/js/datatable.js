7f9a3abbfe616a5fc29264de37df489feb026d0a
Steven Wu <steven.j.wu@gmail.com>
2015-02-11 21:27:05 +1030

    Created command button and plugged into bootgrid. Still need to figure out how to pop up a dialog box.

--- WebContent/js/datatable.js
+++ WebContent/js/datatable.js
@@ -7,7 +7,7 @@
 var rowIds = [];
 
 // Bootgrid widget
-$("#grid").bootgrid({
+var grid = $("#grid").bootgrid({
 	selection : true,
 	multiSelect : true,
 	formatters: {
@@ -18,30 +18,46 @@ $("#grid").bootgrid({
 		"name": function(column, row) {
 			var cellValue = row[column.id];
 			return "<a href=\"www.google.com.au\">" + cellValue + "</a>";
+		},
+		
+		"commands": function(column, row) {
+			return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" 
+			+ row.id + "\"><span class=\"fa fa-pencil\"></span></button>";
 		}
 	}
 }).on("selected.rs.jquery.bootgrid", function(e, rows) {
 	for (var i = 0; i < rows.length; i++) {
 		rowIds.push(rows[i].id);
 	}
-	// Pass selected rowIds from bootgrid widget to another javascript
-	// function
+	// Pass selected rowIds from bootgrid widget to another javascript function.
 	print(rowIds);
 }).on("deselected.rs.jquery.bootgrid", function(e, rows) {
 	for (var i = 0; i < rows.length; i++) {
 		rowIds.splice($.inArray(rows[i].id, rowIds), 1);
 	}
+}).on("loaded.rs.jquery.bootgrid", function() {
+	grid.find(".command-edit").on("click", function(e) {
+		alert("Replace the alert with dialog box.");
+	});
 });
 
 function print(rowIds) {
 	console.log("Printing selected row ID(s): " + rowIds.join(","));
 }
 
-$(function() {
-	$("#drop-down").change(function() {
+$("#modal-dialog").dialog({	
+	    modal: true,
+	    draggable: false,
+	    resizable: false,
+	    position: ['center', 'top'],
+	    show: 'blind',
+	    hide: 'blind',
+	    width: 400
+});	
+
+$("#drop-down").change(function() {
 		var selected = $("#drop-down").val();
 		console.log("selected item in drop-down: " + selected);
-	});
 });
 
 function getCellValue(column, row) {
